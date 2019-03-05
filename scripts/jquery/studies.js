b862e04794071bd978f79c20879f9fe4337a22f0
Modern Coding <cormorant.programming@gmail.com>
2019-03-05 18:07:39 +0700

    Fixing + new stuffs for tomorrow

--- scripts/jquery/studies.js
+++ scripts/jquery/studies.js
@@ -3,29 +3,22 @@
 // });
 
 $(function(){
-  alert("Hello!");
-  var trs = $('tr');
-  var highlighteds = $('.highlighted');
-  var myTd = $('#my-td');
-  $('tr').on('mouseover' , function (event){
-    event.preventDefault();
-    $('.highlighted').removeClass('highlighted');
-    $(this).addClass('highlighted');
+  // $('tr').on('mouseover' , function (event){
+  //   event.preventDefault();
+  //   $('.highlighted').removeClass('highlighted');
+  //   $(this).addClass('highlighted');
+  // });
+//------------------//
+  $('td').on('mouseover', function(event) {
+     event.preventDefault();
+     $('#my-td').html(`<span class="my-td-span">${$(this).text()}</span>`);
   });
-//------------------//
-$('td').on('mouseover', function(event) {
-   event.preventDefault();
-   $('#my-td').html('<span>' +
-        $(this).text() +
-        '</span>'
-  );
-});
 
-$('td').on('mouseout', function(event) {
-   event.preventDefault();
-   $('#my-td').html(null);
-});
-//------------------//
+  $('td').on('mouseout', function(event) {
+     event.preventDefault();
+     $('#my-td').html(null);
+  });
+  //------------------//
 
 
 });
--- scripts/studies.js
+++ scripts/studies.js
@@ -1,8 +1,5 @@
-
 window.onload = function(){
   var tds = document.getElementsByTagName('td');
-  var highlighteds = document.getElementsByClassName('highlighted');
-  var myTd = document.getElementById("my-td");
 
   for ( var i = 0 ; i < tds.length  ; i++){
     tds[i].addEventListener('click', function(event){
@@ -18,13 +15,12 @@ window.onload = function(){
     });
   }
 
-window.onload = function(){
+
   var trs = document.getElementsByTagName('tr');
-  var highlighteds = document.getElementsByClassName('highlighted');
   var myTd = document.getElementById("my-td");
 
-  for ( var i = 0 ; i < trs.length  ; i++){
-    tds[i].addEventListener('mouseover', function(event){
+  for (var i = 0 ; i < trs.length  ; i++){
+    trs[i].addEventListener('mouseover', function(event){
       event.preventDefault();
 
       var highlighteds = document.getElementsByClassName('highlighted');
