e17745c49b8608b6289370cdcbf713e9a0a71874
0905534230 <quan.hoang170203@vnuk.edu.vn>
2019-03-08 10:45:34 +0700

    Hong Hieu

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -3,4 +3,16 @@
 $(function(){
   // logScreenSize();
   resizeElements(['.my-button-people-name', '.my-button-jump-to-people'])
+  if(!modernizr.mq('(min-width:576px)')){
+    $('my-button-people-name').css('width' , '180px')
+  }
+  $('.my-button-people-name').on('click' , function(){
+    // $(this).removeClass("btn-secondary").addClass('btn-danger');
+    $(this).toggleClass('btn-secondary btn-danger');
+    var $button =  $(this).closest('div').find('.my-button-people-name');
+    $button.toggleClass('d-none');
+
+    var id = $button.find('a').attr('href');
+             $(id).toggleClass('d-none') ;
+  });
 });
